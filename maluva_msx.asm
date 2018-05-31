ba8c1b1450246474a9bd52c03fc6800533062d20
Uto <utodev@gmail.com>
2018-06-01 00:41:35 +0200

    first MSX code uploaded

---
+++ maluva_msx.asm
@@ -0,0 +1,304 @@
+
+; MALUVA for MSX (C) 2018 FX, Armando Perez Abad & Uto
+; MIT License applies, see LICENSE file
+; TO BE COMPILED WITH SJASMPLUS
+; Thanks to Boriel for his 8 bits division code in ZX-Basic, DivByTen function is based on his code
+
+                        ORG $013C
+                        OUTPUT "MLV_MSX.BIN"
+
+
+
+; ********************************************************************                        
+;                           CONSTANTS 
+; *******************************************************************
+
+
+                        
+                        define BDOS                      $0005   ; MSXDOS function calls
+                        define FCB                       $005C   ; File control block address
+
+                        define RG1SAV                    $f3e0   ; VDP copy 1
+
+
+
+                        define F_OPEN                    $0F
+                        define F_CLOSE                   $10
+                        define F_CREATE                  $16
+                        define F_SET_TRANSFER_ADDRESS    $1A
+                        define F_RANDOM_BLOCK_READ       $27
+                        define F_RANDOM_BLOCK_WRITE      $26
+
+                        define DAAD_READ_FILENAME       $BFE3
+                        define DAAD_SYSMESS             $B402
+                        define DAAD_FILENAME_ADDR       $C011
+                        
+
+Start                   
+                        DI
+; ---- Preserve registers
+                        PUSH    BC
+                        PUSH    IX
+
+
+; --- Check function selected
+                        LD      D, A
+                        LD      A, (BC)
+
+                        OR      A
+                        JR      Z, loadImg
+                        CP      1
+                        JP      Z, saveGame
+                        CP      2
+                        JP      Z, loadGame
+                        JP      cleanExit
+
+
+; ---- Set the filename
+
+loadImg                 LD      A, D
+                        CALL    DivByTen
+                        ADD     '0'
+                        LD      HL, ImageFilename + 2
+                        LD      (HL),A
+                        LD      A, D
+                        CALL    DivByTen
+                        ADD     '0'
+                        DEC     HL
+                        LD      (HL),A
+                        DEC     HL
+                        LD      A, '0'
+                        ADD     D
+                        LD      (HL),A
+
+
+; --- Open file        
+                        LD      HL, ImageFilename                    
+                        CALL    openFile                        ; Prepares the FCB and opens the file
+                        OR      A                               ; On failure to open, exit
+                        JR      NZ, cleanExit
+
+                        
+
+
+; --- Disable Screen
+                        LD      A, (RG1SAV)
+                        AND     $BF
+                        LD      (RG1SAV), A
+                        OUT     ($99),A
+                        LD      A, $81
+                        OUT     ($99),A
+
+
+; --- Patch FCB so record size for reading is 1, and define where to read to when reading
+                        LD      DE, VRAM_BUFFER
+                        CALL    setFCBParams
+
+                        LD      HL, 1                                   ; Blocks to read
+                        LD      C, F_RANDOM_BLOCK_READ
+                        LD      DE, FCB
+                        CALL    BDOS
+
+
+; ----  Calculate number of lines divided by 8
+
+                        LD      A, (VRAM_BUFFER)
+                        SRL     A
+                        SRL     A
+                        SRL     A
+
+
+; --- And load from file, pixels data first, then atributes
+                        PUSH    AF                              ; Preserve A value
+                        LD      DE, 0                           ; Points to VRAM  (pixel data)
+                        CALL    CopyToVRAM
+                        POP     AF
+                        LD      DE, 02000h                      ; Points to VRAM  (attributes data)
+                        CALL    CopyToVRAM
+
+; ----  Enable Screen
+                        LD      A, (RG1SAV)
+                        OR      $40
+                        LD      (RG1SAV), A
+                        OUT     ($99),A
+                        LD      A, $81
+                        OUT     ($99),A
+
+; ----- Close file
+                        CALL    closeFile
+
+cleanExit               POP     IX
+                        POP     BC
+                        EI
+                        RET
+
+
+; ******************* SAVE AND LOAD GAME ROUTINES   *************************************************
+
+loadGame                CALL    prepareSaveGame                 ; Reads file name and stores at SavegameFilename properly
+                        LD      HL, SavegameFilename            
+                        CALL    openFile                        ; Sets the FCB and opens
+                        OR      A                               ; On failure to open, exit
+                        JR      NZ, diskFailure
+                        POP     DE                              ; Get IX from stack, and pushes it again. IX points to where flags/object locations are located in RAM
+                        PUSH    DE
+                        CALL    setFCBParams                    ; Set to read in 1 byte blocks at DE
+
+                        LD      HL, 512                         ; Blocks to read, 512 bytes
+                        LD      C, F_RANDOM_BLOCK_READ
+                        LD      DE, FCB
+                        CALL    BDOS
+                        CALL    closeFile
+                        JR      cleanExit
+
+
+saveGame                CALL    prepareSaveGame                 ; Reads file name and stores at SavegameFilename properly
+                        LD      HL, SavegameFilename            ; Opens the file and sets the FCB
+                        CALL    createFile
+                        OR      A                               ; On failure to open, exit
+                        JR      NZ, diskFailure
+                        POP     DE                              ; Get IX from stack, and pushes it again. IX points to where flags/object locations are located in RAM
+                        PUSH    DE
+                        CALL    setFCBParams                    ; Set to write in 1 byte blocks at DE
+
+                        LD      HL, 512                         ; Blocks to read, 512 bytes
+                        LD      C, F_RANDOM_BLOCK_WRITE
+                        LD      DE, FCB
+                        CALL    BDOS
+                        CALL    closeFile
+                        JR      cleanExit
+
+
+
+diskFailure             LD      A, 57                           ; E/S error sysmess
+                        LD      HL, $B456                       ; in MSX condacts routies has no RET at the end, they jump to pipeline $B17C again
+                        LD      (HL),201                        ; as we need it to return, we are patching temporaryly the SYSMES code. This replaces POP HL with RET
+                        PUSH    HL
+                        CALL    DAAD_SYSMESS
+                        POP     HL                              
+                        LD      (HL),03                         ; and this restores POP HL
+                        JR      cleanExit                        
+
+prepareSaveGame         CALL    DAAD_READ_FILENAME              ; request a file name from input 
+                        LD      HL, DAAD_FILENAME_ADDR          ; copies 8 characters to SavegameFilename where ther eextension is already present
+                        LD      DE, SavegameFilename            ; DAAD routine alrady stores a 8 byte length file name at DAAD_FILENAME_ADDR and fills with spaces if needed
+                        LD      BC, 8                           
+                        LDIR
+                        RET
+
+; ********************************************************************                        
+;                           AUX FUNCTIONS
+; *******************************************************************
+
+;- -------------------------------------------------------------------------------------------
+; Input: DE= address to read/write to, it's input to F_SET_TRANSFER_ADDRESS
+setFCBParams            LD      HL, 1                           ; SET read block size to 1 byte
+                        LD      (FCB + 14), HL            
+
+                        LD      C, F_SET_TRANSFER_ADDRESS       ; Set where to read to (or write to )
+                        CALL      BDOS
+                        RET
+
+
+; ---------------------------------------------------------------------------
+; DE    Direccion destino en VRAM
+; A     Lineas a copiar
+CopyToVRAM
+; VRAM address setup
+                        PUSH    AF
+                        LD      A,E
+                        OUT     ($99),A
+                        LD      A, D
+                        OR      $40
+                        OUT     ($99),A
+                        POP     AF
+
+copyToVRAMLoop          PUSH    AF                              ;lines to load
+
+                        LD      C, F_SET_TRANSFER_ADDRESS
+                        LD      DE, VRAM_BUFFER                 ; where to load them
+                        CALL    BDOS
+
+                        LD      HL, 100H                        ; Blocks to read = bytes to read
+                        LD      C, F_RANDOM_BLOCK_READ
+                        LD      DE, FCB
+                        CALL    BDOS
+
+                        LD      HL, VRAM_BUFFER
+                        LD      C, $98
+                        LD      B, $FF
+                        OTIR                                    ; Copy 255 bytes
+                        OUTI                                    ; Copy one more byte, up to 256
+
+
+                        POP     AF
+                        DEC     A
+                        JR      NZ, copyToVRAMLoop
+                        RET
+
+;----------------------------------------------------------------------------
+prepareFCB              PUSH    HL
+clearFCB                LD      HL, FCB                       ; Fills FCB with Zeroes
+                        XOR     A
+                        LD      (HL), A
+                        LD      DE, FCB +1
+                        LD      BC, 1Eh + 11
+                        LDIR                   
+                        POP     HL                      ; And copies Filename on FCB to open/create
+                        LD      DE, FCB + 1           
+                        LD      BC, 11
+                        LDIR                                                           
+                        RET
+
+
+; openFile -----------------------------------------------------------------
+;input:   HL --> points to 11 chars filename
+openFile                CALL    prepareFCB
+                        LD      C, F_OPEN
+                        LD      DE, FCB
+                        CALL    BDOS                  
+                        RET
+
+; closefile --------------------------------------------------------------------------------------------------------------
+closeFile               LD      C, F_CLOSE
+                        LD      DE, FCB
+                        CALL    BDOS                  
+                        RET
+
+; createFile -----------------------------------------------------------------
+;input:   HL --> points to 11 chars filename
+createFile              CALL    prepareFCB
+                        LD      C, F_CREATE
+                        LD      DE, FCB
+                        CALL    BDOS                  
+                        RET
+
+
+
+; ---------------------------------------------------------------------------
+; *** Divides A by 10 and returns the remainder in A and the quotient in D^***
+DivByTen
+                        LD      D, A           ; Does A / 10
+                        LD      E, 10          ; At this point do H / 10
+                        LD      B, 8
+                        XOR     A              ; A = 0, Carry Flag = 0
+DivByTenLoop
+                        SLA     D
+                        RLA            
+                        CP      E               
+                        JR      C, DivByTenNoSub
+                        SUB     E               
+                        INC     D               
+DivByTenNoSub
+                        DJNZ    DivByTenLoop
+                        RET                    ;A= remainder, D = quotient
+
+
+; ---------------------------------------------------------------------------
+ImageFilename           DB      "000     MS2"
+SavegameFilename        DB      "UTO     SAV"        
+
+VRAM_BUFFER
+                        DS      $100
+
+; ---------------------------------------------------------------------------
---
+++ msxdos_loader.asm
@@ -0,0 +1,280 @@
+
+; MALUVA for MSX (C) 2018 FX, Armando Perez Abad & Uto
+; MIT License applies, see LICENSE file
+; TO BE COMPILED WITH SJASMPLUS
+
+
+                        OUTPUT "msxdaad.com"
+
+; ********************************************************************                        
+;                           CONSTANTS 
+; *******************************************************************
+                        define  Start                    $0100
+                        define  Start2                   $8100
+
+
+                        define  FORCLR                   $f3e9
+                        define  BAKCLR                   $f3ea
+                        define  BDRCLR                   $f3eb
+                        define  EXPTBL                   $fcc1
+                        define  RAMAD0                   $f341          
+
+                        ;For MSXDOS
+                        define  BDOS                     $0005
+                        define  FCB                      $005C
+                        define  F_SET_TRANSFER_ADDRESS   $1A
+                        define  F_RANDOM_BLOCK_READ      $27
+                        define  F_OPEN                   $0F
+                        define  F_CLOSE                  $10
+
+                        define  CurrentSlotAddress       $D053         ; This is te one used by DAAD, otherwise use $FFE0
+
+                        define  VRAM_BUFFER              $7400
+
+                        define  GameStart                $cff3        ; Address to start the game after the OUT(A8)
+
+                        define  PatchSetROM              $cf84
+                        define  PatchSetRAM              $cf8f
+
+                        define  PatchInterrupt           $d026        ;Interrupt address 
+
+
+;------------------------------------------------------------------------
+
+                        ORG             Start
+
+
+; ---- Copy code to real execution address (where is safe to run)
+
+                        DI
+                        LD      HL, Start2Copy
+                        LD      DE, Start2
+                        LD      BC, End2Copy - Start2Copy
+                        LDIR
+                        JP      Start2
+
+;---------------------------------------------------------
+
+Start2Copy
+
+                        phase   Start2
+
+; ---- Load FILES.BIN
+			      LD      DE, FilesBinContent	; Load FILES.BIN at FilesBinContent if avaliable
+                        LD      HL, FilesBinFileName	
+                        CALL    MSXDOS1Load
+
+; ---- MDG files should be loaded so last byte is at $AFFF, that is, $B000 minus file size, but as we don't know size beforehand, we load it at $100, then move it to proper position
+
+                        LD      DE, $100		            ; Load graphics database at $100 (temporary location)
+                        LD 	  HL, FilesBinContent		; Filename pointed by first file in FILES.BIN
+                        CALL    MSXDOS1Load
+
+                        LD 	  BC, (FCB + 10h)		       ; Position of real number of bytes readed
+                        LD 	  HL, $B000		
+                        OR 	  A			              ; Clear Carry flag
+                        SBC	  HL, BC			 
+                        LD      D, H
+                        LD      E, L                        ; DE = $B000 - file size
+                        PUSH    DE                          ; Save position of  MDG for later use
+                        LD 	  HL, $100		
+                        LDIR 				            ; Move MDG to proper position
+
+; ----- Load database at address $100
+                        LD      HL, FilesBinContent + 11
+                        LD      DE, $100
+                        CALL    MSXDOS1Load
+
+; ----- Load interpreter at address $B000
+
+                        LD      HL, FilesBinContent + 22
+                        LD      DE, $B000
+                        CALL    MSXDOS1Load
+
+; ------ Multiple patchs on interpreter                        
+
+PatchCode	            LD      A, $F3                         ;EI
+                        LD      (PatchSetRAM), A
+
+                        LD      A, $CD                         ;CALL
+                        LD      (PatchSetROM), A
+                        LD      (PatchSetRAM + 1), A
+
+                        LD      A, $FB                         ;EI
+                        LD      (PatchSetROM + 3), A
+
+                        LD      A, $C9                         ;RET
+                        LD      (PatchSetROM + 4), A
+                        LD      (PatchSetRAM + 4), A
+
+                        LD      HL, ActivateBIOS
+                        LD      (PatchSetROM + 1), HL
+
+                        LD      HL, ActivateGame
+                        LD      (PatchSetRAM + 2), HL
+
+                        LD      A, $56                         ;IM1 instead of IM2
+                        LD      (PatchInterrupt), A
+
+                        POP     HL                                ; Restore position where MDG was loaded
+                        DEC     H                                 ; Point  256 bytes below
+                        LD      ($C453), HL                       ; Patch all scroll and copy routines
+                        LD      ($C465), HL
+                        LD      ($C50C), HL
+                        LD      ($C522), HL
+                        LD      ($C53B), HL
+                        LD      ($C54F), HL
+                        LD      ($C649), HL
+                        LD      ($CCF7), HL
+                        LD      ($CD1B), HL
+
+                        LD       A, $F                            ; Changes screen colors
+                        LD       (FORCLR), A
+                        LD       A, 1
+                        LD       (BAKCLR), A
+                        LD       (BDRCLR), A
+
+                        LD       A, $FF                         ;Tells the current selected slot is the game slot, not BIOS slot
+                        LD       (CurrentSlotAddress), A        ; No need to preserve it, the jumping routine does it
+
+
+                        JP       GameStart                      ; Run game!
+
+
+; --- MSXDOS1Load -----------------------------------------------------------
+;Input:	 HL	 Points to filename
+;	 DE 	 Destination Address
+; Output: Carry flag is set on return in case of failure
+; ---------------------------------------------------------------------------
+MSXDOS1Load		      PUSH 	    DE			; Preserve destintion address
+			      PUSH 	    HL	 		; Preserve file name 
+                        
+
+                        LD        DE, FCB			; Fills FCB with Zeroes
+                        XOR       A
+                        LD        (DE), A
+                        LD        H, D
+                        LD        L, E
+                        INC       DE
+                        LD        BC, 1Eh + 11
+                        LDIR				
+
+                        POP 	    HL			; Copy Filaname into FCB to conform an unopened FCB
+                        LD        DE, FCB + 1		
+                        LD        BC, 11
+                        LDIR				                     			
+
+                        LD        C, F_OPEN
+                        LD        DE, FCB
+                        CALL      BDOS
+
+                        POP 	    DE			; restore destination address
+                        OR        A
+                        RET       NZ			
+
+ 
+ReadFile                LD        C, F_SET_TRANSFER_ADDRESS
+	                  CALL      BDOS
+                
+			      LD        HL, 1
+                        LD        (FCB + 14), HL         ; set record size = 1
+
+	                  LD 	    HL, $FFFF		; Read as much bytes as possible
+                        LD        C, F_RANDOM_BLOCK_READ
+                        LD        DE, FCB
+                        CALL      BDOS
+
+CloseFile               LD        C, F_CLOSE
+                        LD        DE, FCB
+                        CALL      BDOS			
+                        RET
+               
+; ---------------------------------------------------------------------------
+ActivateBIOS
+                        PUSH    HL
+                        PUSH    DE
+                        PUSH    BC
+                        LD      A, (EXPTBL)
+                        CALL    SetSlotOnPage0
+                        XOR     A
+                        LD      (CurrentSlotAddress), A
+                        POP     BC
+                        POP     DE
+                        POP     HL
+                        RET
+
+; ---------------------------------------------------------------------------
+ActivateGame
+                        PUSH    HL
+                        PUSH    DE
+                        PUSH    BC
+                        LD      A, (RAMAD0)
+                        CALL    SetSlotOnPage0
+                        LD      A, $FF
+                        LD      (CurrentSlotAddress), A
+                        POP     BC
+                        POP     DE
+                        POP     HL
+                        RET
+
+; ---------------------------------------------------------------------------
+
+; ---------------------------
+; SetSlotOnPage0
+; Sets Slot received as parameter as page 0 in the Z80 memory address space
+; A = Slot in  FxxxSSPP format
+; ----------------------------
+            
+
+SetSlotOnPage0          DI
+                        LD      B,A
+                        IN      A,(#A8)
+                        AND     11111100b
+                        LD      D,A
+                        LD      A,B
+                        AND     #03
+                        OR      D
+                        LD      D,A
+                        OUT     (#A8),A
+                        LD      A,B
+                        BIT     7,A
+                        RET     Z
+                        AND     #03
+                        RRCA
+                        RRCA
+                        AND     11000000b
+                        LD      C,A
+                        LD      A,D
+                        AND     00111111b
+                        OR      C
+                        LD      C,A
+                        LD      A,B
+                        AND     00001100b
+                        RRCA
+                        RRCA
+                        AND     $03
+                        LD      B,A
+                        LD      A,C
+                        OUT     ($A8),A
+                        LD      A,($FFFF)
+                        CPL
+                        AND     11111100b
+                        OR      B
+                        LD      ($FFFF),A
+                        LD      C,A
+                        LD      A,D
+                        OUT     ($A8),A
+                        RET
+ 
+
+; ---------------------------------------------------------------------------
+FilesBinFileName	      db      "FILES   BIN"
+FilesBinContent	      db      "DAAD    MDG"
+           		      db      "DAAD    DDB"
+           		      db      "DAAD    Z80"
+
+
+        dephase
+
+End2Copy
+        
