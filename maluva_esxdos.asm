2de830e0a2a21e8ebe3c2b94bd71e599f1352406
Uto Dev <csanchez71@gmail.com>
2019-09-05 17:18:20 +0200

    Modified ESXDOS patch so it works with english interpreter too

--- maluva_esxdos.asm
+++ maluva_esxdos.asm
@@ -4,7 +4,7 @@
 ; Thanks to Boriel for his 8 bits division code in ZX-Basic, DivByTen function is based on his code
 
 			ORG $843C
-                        OUTPUT  MLV_ESX.BIN
+            OUTPUT  MLV_ESX.BIN
 	
 
 ; ********************************************************************                        
@@ -21,81 +21,97 @@
 			define FA_WRITE		$02
 			define FA_CREATE_AL	$0C
 
-			define VRAM_ADDR 	  $4000 ; The video RAM address
+			define INTERPRETER_ADDRESS $6000  ; Address where the interpreter is loaded
+
+			define VRAM_ADDR 	  	  $4000 ; The video RAM address
 			define VRAM_ATTR_ADDR     VRAM_ADDR + $1800 ;  points to attributes zone in VRAM 
 
-			define DAAD_READ_FILENAME $7056	; DAAD function to request a file name
-			define DAAD_SYSMESS 	  $6DE8 ; DAAD function to print a system message
+			define DAAD_READ_FILENAME_ES $7056	; DAAD function to request a file name
+			define DAAD_READ_FILENAME_EN $6FF6	; DAAD function to request a file name
 
-			define DAAD_FILENAME_ADDR $70B5 ; DAAD function where the file name read by DAAD_READ_FILENAME is stored
+			define DAAD_SYSMESS_ES 	  $6DE8 ; DAAD function to print a system message
+			define DAAD_SYSMESS_EN 	  $6D94 ; DAAD function to print a system message
 
+			define DAAD_FILENAME_ADDR_ES $70B5 ; DAAD function where the file name read by DAAD_READ_FILENAME_ES is stored
+			define DAAD_FILENAME_ADDR_EN $7055 ; DAAD function where the file name read by DAAD_READ_FILENAME_ES is stored
 
 ; ********************************************************************                        
 ;                                 MAIN
 ; ********************************************************************
 
 Start			
-			DI
-			PUSH 	BC
-			PUSH 	IX
-			
-			LD 	D, A		; Preserve first parameter
-			LD 	A, (BC)		; Get second parameter (function number) on A
+					DI
+					PUSH 	BC
+					PUSH 	IX
 
-			OR 	A
-			JR 	Z, LoadImg
-			CP 	1
-			JP 	Z, SaveGame
-			CP 	2
-			JP 	Z, LoadGame
-			JP 	cleanExit
+; ------ Detect if english or spanish interpreter
+					PUSH 	AF
+					PUSH 	BC
+					LD 		A, (INTERPRETER_ADDRESS+1)
+					CP 		$55			
+					JR		NZ, Spanish				 ; In english interpreter, the patch will be applied everytime the extern is called. Although that may 
+					CALL 	PatchForEnglish    		 ; look like losing time, I prefered keeping the code as smaller as possible, so I don't check if patch 
+Spanish				POP		BC						 ; already applied. It takes milliseconds and no one could notice when playing anyway
+					POP 	AF
+
+			
+Init				LD 	D, A		; Preserve first parameter
+					LD 	A, (BC)		; Get second parameter (function number) on A
+
+					OR 	A
+					JR 	Z, LoadImg
+					CP 	1
+					JP 	Z, SaveGame
+					CP 	2
+					JP 	Z, LoadGame
+					JP 	cleanExit
 ; ---- Set the filename
 LoadImg
-			LD 	A, D		; Restore first parameter
-			CALL 	DivByTen
-			ADD 	'0'
-			LD 	HL, Filename+2
-			LD 	(HL),A
-			LD 	A, D
-			CALL 	DivByTen
-			ADD 	'0'
-			DEC 	HL
-			LD 	(HL),A
-			DEC 	HL
-			LD 	A, '0'
-			ADD 	D
-			LD 	(HL),A
+					LD 	A, D		; Restore first parameter
+					CALL 	DivByTen
+					ADD 	'0'
+					LD 	HL, Filename+2
+					LD 	(HL),A
+					LD 	A, D
+					CALL 	DivByTen
+					ADD 	'0'
+					DEC 	HL
+					LD 	(HL),A
+					DEC 	HL
+					LD 	A, '0'
+					ADD 	D
+					LD 	(HL),A
 
 
 ; --- Set default disk  
-			CALL 	setDefaultDisk
-                        JR      C, cleanExit
+					CALL 	setDefaultDisk
+                    JR      C, cleanExit
 
 
 ; --- open file
-                        LD      B, FA_READ   
-			LD   	IX, Filename
-			RST     $08
-                        DB      F_OPEN      
-                        JR      C, cleanExit
+                    LD      B, FA_READ   
+					LD   	IX, Filename
+					RST     $08
+                    DB      F_OPEN      
+                    JR      C, cleanExit
 
 
 ;  --- From this point we don't check read failure, we assume the graphic file is OK. Adding more fail control would increase the code size and chances of fail are low from now on
 
 
 ; --- Preserve A register, containing the file handle 
-                        LD      D,A	
+                    LD      D,A	
 
 ; --- read header
-                        LD 	IX, IMGNumLine
-                        PUSH 	DE
-                        PUSH 	BC
-                        LD      BC, 1
-                        RST     $08
-                        DB      F_READ     
-                        POP 	BC
-                        POP 	DE
-                        LD 	A, (IMGNumLine) ; A register contains number of lines now
+					LD 	IX, IMGNumLine
+					PUSH 	DE
+					PUSH 	BC
+					LD      BC, 1
+					RST     $08
+					DB      F_READ     
+					POP 	BC
+					POP 	DE
+					LD 	A, (IMGNumLine) ; A register contains number of lines now
                                             
 
 ; read data - for Spectrum we start by reading  as much thirds of screen as possible, in the first byte of file the number of lines appears, so if there is carry when comparing to 64,
@@ -103,29 +119,29 @@ LoadImg
 ;	      screen. 
 
 			
-			SUB 	64
-			JR 	C, drawPartialThird  ; if thre is no carry, there is at least one whole third of screen
-			LD 	BC, 2048
-			LD 	H, B
-			LD 	L, C
-			SUB 	64
-			JR 	C, drawWholeThirds   ;if there is still no carry, there are at least two thirds of screen
-			ADD 	HL, BC
+					SUB 	64
+					JR 	C, drawPartialThird  ; if thre is no carry, there is at least one whole third of screen
+					LD 	BC, 2048
+					LD 	H, B
+					LD 	L, C
+					SUB 	64
+					JR 	C, drawWholeThirds   ;if there is still no carry, there are at least two thirds of screen
+					ADD 	HL, BC
 nextThird	        SUB 	64
-			JR 	C, drawWholeThirds ; if there is still no carry, it's a full screen (3 thirds)
-			ADD	HL, BC	           ; read one, two or the three whole thirds
+					JR 	C, drawWholeThirds ; if there is still no carry, it's a full screen (3 thirds)
+					ADD	HL, BC	           ; read one, two or the three whole thirds
 drawWholeThirds		LD 	B, H
-			LD 	C, L		
-			LD 	IX, VRAM_ADDR
-			PUSH    AF
-			LD 	A, D 		; file handle
-			PUSH 	DE
-                        RST     $08
-                        DB      F_READ
-                        POP 	DE
-                        POP 	AF
-                        LD 	IX, VRAM_ADDR
-                        ADD 	IX, BC		; Hl points to next position in VRAM to paint at
+					LD 	C, L		
+					LD 	IX, VRAM_ADDR
+					PUSH    AF
+					LD 	A, D 		; file handle
+					PUSH 	DE
+					RST     $08
+					DB      F_READ
+					POP 	DE
+					POP 	AF
+					LD 	IX, VRAM_ADDR
+					ADD 	IX, BC		; Hl points to next position in VRAM to paint at
                         
 ; --- This draws the last third, that is an uncomplete one, to do so, the file will come prepared so instead of an standar third with 8 character rows, it's a rare third with less rows.
 ;     For easier undesrtanding we will call 'row" each character row left, and 'line' each pixel line left, so each row is built by 8 lines.
@@ -135,103 +151,103 @@ drawWholeThirds		LD 	B, H
 ;     BC register,and then just read BC bytes 8 times, increasin IX (pointing to VRAM) by 256 each time to point to the next line inside the row
 
 
-drawPartialThird        ADD	A, 64		; restore the remaining number of lines (last SUB went negative)                
-                        OR 	A
-                        JR 	Z,readAttr	; if A = 0, then there were exactly  64, 128, or 192 lines, just jump to attributes section	
-			ADD	A, A
-			ADD	A, A		; A=A*4. Will never exceed 1 byte as max value for lines is 63, and 63*4 = 252
-			LD 	B, 0
-			LD 	C, A		; BC = number of bytes to read each time (numlines/ 8 x 32). 
-			LD 	E, 8		; Times to do the loop, will be used as counter. We don't use B and DJNZ cause we need BC all the time and in the end is less productive
-drawLoop		LD 	A, D 		; file handle
-			PUSH 	DE
-			PUSH 	IX
-			RST     $08
-                        DB      F_READ
-                        POP 	IX
-                        INC  	IXH		; Increment  256 to point to next line address
-                        POP 	DE
-                        DEC 	E
-                        JR 	NZ, drawLoop
+drawPartialThird    ADD	A, 64		; restore the remaining number of lines (last SUB went negative)                
+                    OR 	A
+                    JR 	Z,readAttr	; if A = 0, then there were exactly  64, 128, or 192 lines, just jump to attributes section	
+					ADD	A, A
+					ADD	A, A		; A=A*4. Will never exceed 1 byte as max value for lines is 63, and 63*4 = 252
+					LD 	B, 0
+					LD 	C, A		; BC = number of bytes to read each time (numlines/ 8 x 32). 
+					LD 	E, 8		; Times to do the loop, will be used as counter. We don't use B and DJNZ cause we need BC all the time and in the end is less productive
+drawLoop			LD 	A, D 		; file handle
+					PUSH 	DE
+					PUSH 	IX
+					RST     $08
+					DB      F_READ
+					POP 	IX
+					INC  	IXH		; Increment  256 to point to next line address
+					POP 	DE
+					DEC 	E
+					JR 	NZ, drawLoop
 
 ; read the attributes 
 
-readAttr		XOR 	A
-			LD	H, A
-			LD 	A, (IMGNumLine)	; restore number of lines
-			LD 	L, A			; now HL = number of lines 
-			ADD 	HL, HL
-			ADD 	HL, HL			; Multiply by 4 (32 bytes of attributes per each 8 lines  = means 4 per line)
-			PUSH 	HL
-			POP 	BC
-			LD 	IX, VRAM_ATTR_ADDR	; attributes VRAM
-			LD 	A, D 			; file handle
-			PUSH	DE
-			RST 	$08
-			DB 	F_READ
-			POP	DE
+readAttr			XOR 	A
+					LD	H, A
+					LD 	A, (IMGNumLine)	; restore number of lines
+					LD 	L, A			; now HL = number of lines 
+					ADD 	HL, HL
+					ADD 	HL, HL			; Multiply by 4 (32 bytes of attributes per each 8 lines  = means 4 per line)
+					PUSH 	HL
+					POP 	BC
+					LD 	IX, VRAM_ATTR_ADDR	; attributes VRAM
+					LD 	A, D 			; file handle
+					PUSH	DE
+					RST 	$08
+					DB 	F_READ
+					POP	DE
 
 ; ---- Close file	
-			LD 	A, D
-			RST     $08
-                        DB      F_CLOSE
+					LD 	A, D
+					RST     $08
+                    DB      F_CLOSE
 	
-cleanExit		EI
-			POP 	IX
-			POP 	BC
-			RET
+cleanExit			EI
+					POP 	IX
+					POP 	BC
+					RET
 
 ; Both read savegame and load savegame use the same code, that is just slightly modified before jumping in the common part at DoReadOrWrite
 
-LoadGame		LD  	A, FA_READ
-			LD 	(OpenMode+1),A
-			LD  	A, F_READ
-			LD 	(ReadWrite),A
-			JR	DoReadOrWrite
+LoadGame			LD  A, FA_READ
+					LD	(OpenMode+1),A
+					LD  A, F_READ
+					LD 	(ReadWrite),A
+					JR	DoReadOrWrite
 
-SaveGame		LD  	A, FA_CREATE_AL
-			LD 	(OpenMode+1),A
-			LD  	A, F_WRITE
-			LD 	(ReadWrite),A
+SaveGame			LD  A, FA_CREATE_AL
+					LD 	(OpenMode+1),A
+					LD  A, F_WRITE
+					LD 	(ReadWrite),A
 
 
 DoReadOrWrite		
-			EI
-			CALL 	DAAD_READ_FILENAME	; Ask for file name
-			DI
-			CALL 	cleanSaveName		; Convert DAAD 10 filename into 8.3 filename
+					EI
+ReadFilenameCall	CALL	DAAD_READ_FILENAME_ES	; Ask for file name
+					DI
+					CALL 	cleanSaveName		; Convert DAAD 10 filename into 8.3 filename
 
 ; --- Set default disk  
-			CALL 	setDefaultDisk
-                        JR      C, diskFailure
+					CALL 	setDefaultDisk
+                    JR      C, diskFailure
 
 ; --- open file
-OpenMode                LD      B, FA_READ		; May be modified by FA_CREATE_AL above
-			LD   	IX, SaveLoadFilename
-			RST     $08			
-                        DB      F_OPEN      
-                        JR      C, diskFailure
-                        LD 	(SavegameHandle+1), A
+OpenMode            LD      B, FA_READ		; May be modified by FA_CREATE_AL above
+					LD   	IX, SaveLoadFilename
+					RST     $08			
+					DB      F_OPEN      
+					JR      C, diskFailure
+					LD 	(SavegameHandle+1), A
 ; --- read or write file
 
-			POP	IX			; Gets IX value back from stack, then push again
-			PUSH 	IX
-			LD 	BC, 512			; Save flags and objects
- 			RST     $08
-ReadWrite               DB      F_READ     		; May be modified by F_WRITE ABOVE
-			JR	C, diskFailure
+					POP	IX			; Gets IX value back from stack, then push again
+					PUSH 	IX
+					LD 	BC, 512			; Save flags and objects
+ 					RST     $08
+ReadWrite           DB      F_READ     		; May be modified by F_WRITE ABOVE
+					JR	C, diskFailure
                       
 
 SavegameHandle		LD 	A, $FF			; That $FF will be modifed by code above
-			RST     $08
-                        DB      F_CLOSE
+					RST     $08
+                    DB      F_CLOSE
 
 
-			JR 	cleanExit	
+					JR 	cleanExit	
 
-diskFailure		LD 	L, 57			; E/S error
-			CALL    DAAD_SYSMESS
-			JR 	cleanExit
+diskFailure			LD 	L, 57			; E/S error
+DAADSysmesCall		CALL    DAAD_SYSMESS_ES
+					JR 	cleanExit
 			
 
 
@@ -241,45 +257,55 @@ diskFailure		LD 	L, 57			; E/S error
 ; *******************************************************************
 
 ; *** Makes filename read by DAAD to be compliant with ESXDOS 8+3 filenames***
-cleanSaveName		LD 	HL, DAAD_FILENAME_ADDR			; address of filename requested
-			LD 	BC, $08FF			; we will use LDI together with DJNZ, B will work for DJNZ but we set C to $FF
-			LD 	DE, SaveLoadFilename
-cleanSaveNameLoop	LD 	A, (HL)				; to avoid LDI decrement of BC to affect B
-			OR 	A
-			JR	Z, cleanSaveSetExtension
-			CP 	$20
-			JR	Z, cleanSaveSetExtension
-			LDI
-			DJNZ 	cleanSaveNameLoop
+cleanSaveName			LD 	HL, DAAD_FILENAME_ADDR_ES			; address of filename requested
+						LD 	BC, $08FF			; we will use LDI together with DJNZ, B will work for DJNZ but we set C to $FF
+						LD 	DE, SaveLoadFilename
+cleanSaveNameLoop		LD 	A, (HL)				; to avoid LDI decrement of BC to affect B
+						OR 	A
+						JR	Z, cleanSaveSetExtension
+						CP 	$20
+						JR	Z, cleanSaveSetExtension
+						LDI
+						DJNZ 	cleanSaveNameLoop
 cleanSaveSetExtension	LD 	HL, SaveLoadExtension
-			LD 	BC, 5
-			LDIR
-			RET
+						LD 	BC, 5
+						LDIR
+						RET
 
 ; **** Sets the default disk active
-setDefaultDisk		XOR	A  
+setDefaultDisk			XOR	A  
                         RST     $08 
                         DB      M_GETSETDRV
                         RET
 			
 
 ; *** Divides A by 10 and returns the remainder in A and the quotient in D^***
-DivByTen		LD 	D, A			; Does A / 10
-			LD 	E, 10			; At this point do H / 10
-			LD 	B, 8
-			XOR 	A				; A = 0, Carry Flag = 0
-DivByTenLoop		SLA	D
-			RLA			
-			CP	E		
-			JR	C, DivByTenNoSub
-			SUB	E		
-			INC	D		
-DivByTenNoSub		djnz 	DivByTenLoop
-			RET				;A= remainder, D = quotient
+DivByTen				LD 	D, A			; Does A / 10
+						LD 	E, 10			; At this point do H / 10
+						LD 	B, 8
+						XOR 	A				; A = 0, Carry Flag = 0
+DivByTenLoop			SLA	D
+						RLA			
+						CP	E		
+						JR	C, DivByTenNoSub
+						SUB	E		
+						INC	D		
+DivByTenNoSub			DJNZ DivByTenLoop
+						RET				;A= remainder, D = quotient
 
-Filename		DB 	"000.ZXS",0
-IMGNumLine		DB 	0
-SaveLoadFilename	DB 	"PLACEHOL.SAV",0
-SaveLoadExtension	DB 	".SAV", 0
+; ** Function that patches Maluva code so it calls DAAD functions at the proper locations whe the english interpreter has been detected
+PatchForEnglish			LD HL, DAAD_READ_FILENAME_EN
+						LD (ReadFilenameCall+1), HL
+						LD HL, DAAD_SYSMESS_EN
+						LD (DAADSysmesCall+1), HL
+						LD HL, DAAD_FILENAME_ADDR_EN
+						LD(cleanSaveName+1), HL
+						RET
+
+
+Filename				DB 	"UTO.ZXS",0
+IMGNumLine				DB 	0
+SaveLoadFilename		DB 	"PLACEHOLD.SAV",0
+SaveLoadExtension		DB 	".SAV", 0
 
 
