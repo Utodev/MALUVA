81b23fde3c4ae72794a9daf49201b3a22ff5e5af
Uto Dev <csanchez71@gmail.com>
2019-09-17 22:59:08 +0200

    Added support for Spectrum Next

---
+++ maluva_next.asm
@@ -0,0 +1,362 @@
+; MALUVA (C) 2018 Uto
+; MIT License applies, see LICENSE file
+; TO BE COMPILED WITH SJASMPLUS
+; Thanks to Boriel for his 8 bits division code in ZX-Basic, DivByTen function is based on his code
+; Thanks to Ped7g, Alcoholics Anonymous and  SevenFFF for their help in the Spectrum Next forum
+
+			ORG $843C
+            OUTPUT  MLV_NEXT.BIN
+	
+
+; ********************************************************************                        
+;                           CONSTANTS 
+; *******************************************************************
+
+
+			define M_GETSETDRV  	$89
+			define F_OPEN  			$9a
+			define F_CLOSE 			$9b
+			define F_READ  			$9d
+			define F_WRITE 			$9e       
+			define FA_READ 			$01
+			define FA_WRITE			$02
+			define FA_CREATE_AL		$0C
+
+			define ULA_ATTR_ADDR	$5800
+
+			define LAYER2_ACCESS_PORT   $123B
+			
+
+			define REG_LAYER2_RAM_PAGE  $12
+			define REG_TRANSPARENCY	    $14
+			define REG_SPRITE_LAYERS	$15
+			define REG_CLIP_LAYER2		$18
+			define REG_CLIP_WINDOW_CTRL $1C
+			define REG_PALETTE_INDEX    $40
+			define REG_PALETTE_VALUE	$41
+			define REG_PALETTE_CRTL		$43
+			define REG_MMU7			 	$57
+
+
+			define INTERPRETER_ADDRESS $6000  	; Address where the interpreter is loaded
+
+			define DAAD_READ_FILENAME_ES $7056	; DAAD function to request a file name
+			define DAAD_READ_FILENAME_EN $6FF6	; DAAD function to request a file name
+
+			define DAAD_SYSMESS_ES 	  $6DE8 	; DAAD function to print a system message
+			define DAAD_SYSMESS_EN 	  $6D94 	; DAAD function to print a system message
+
+			define DAAD_FILENAME_ADDR_ES $70B5 ; DAAD function where the file name read by DAAD_READ_FILENAME_ES is stored
+			define DAAD_FILENAME_ADDR_EN $7055 ; DAAD function where the file name read by DAAD_READ_FILENAME_ES is stored
+
+			define DAAD_PATCH_ES $707A		    ; Address where the interpreter sets the internal flag which makes the words be cutted when printed
+			define DAAD_PATCH_EN $701A			; Address where the interpreter sets the internal flag which makes the words be cutted when printed	
+
+
+; ********************************************************************                        
+;                                 MAIN
+; ********************************************************************
+
+Start			
+					DI
+					PUSH 	BC
+					PUSH 	IX
+
+; ------ Detect if english or spanish interpreter
+					PUSH 	AF
+					PUSH 	BC
+					LD 		A, (INTERPRETER_ADDRESS+1)
+					CP 		$55			
+					JR		NZ, Spanish				 ; In english interpreter, the patch will be applied everytime the extern is called. Although that may 
+					CALL 	PatchForEnglish    		 ; look like losing time, I prefered keeping the code as smaller as possible, so I don't check if patch 
+					JR 		LangCont
+Spanish				LD 		A, $C9					; (RET)
+					LD      (DAAD_PATCH_ES), A 		; Patching the "ask for file name" routine so it doesn't break the words the nwriting messages afterwards
+LangCont			POP		BC						 ; already applied. It takes milliseconds and no one could notice when playing anyway
+					POP 	AF
+
+			
+Init				LD 	D, A		; Preserve first parameter
+					LD 	A, (BC)		; Get second parameter (function number) on A
+
+					OR 	A
+					JR 	Z, LoadImg
+					CP 	1
+					JP 	Z, SaveGame
+					CP 	2
+					JP 	Z, LoadGame
+					JP 	cleanExit
+; ---- Set the filename
+LoadImg
+					LD 	A, D		; Restore first parameter
+					CALL 	DivByTen
+					ADD 	'0'
+					LD 	HL, Filename+2
+					LD 	(HL),A
+					LD 	A, D
+					CALL 	DivByTen
+					ADD 	'0'
+					DEC 	HL
+					LD 	(HL),A
+					DEC 	HL
+					LD 	A, '0'
+					ADD 	D
+					LD 	(HL),A
+
+
+; --- Set default disk  
+					CALL 	setDefaultDisk
+                    JP      C, cleanExit
+
+
+; --- open file
+                    LD      B, FA_READ   
+					LD   	IX, Filename
+					RST     $08
+                    DB      F_OPEN      
+                    JP      NC, fileOpened
+					XOR 	A
+					LD 		BC, LAYER2_ACCESS_PORT
+					OUT 	(C),A					 ; Disable layer 2
+					JP 		cleanExit
+
+
+
+; --- Preserve A register, containing the file handle 
+fileOpened          LD	    (FileHandle),A
+
+; --- read header - first two bytes (IMGNumLine is just after TransparencyColor in the RAM)
+					LD 	    IX, TransparencyColor
+					LD      BC, 2
+					RST     $08
+					DB      F_READ     
+
+; ---- activate Layer2 (in case it wasn't already active)
+					LD 		A, 2
+					LD 		BC, LAYER2_ACCESS_PORT
+					OUT 	(C),A
+
+
+; ---- set layers order to SLU (Sprite, Layer2, ULA). Actually, unnecesery, as SLU is the default option after reset
+					NEXTREG REG_SPRITE_LAYERS, 0
+
+; ---- place the Layer2 RAM at a higher slot
+					NEXTREG REG_LAYER2_RAM_PAGE, 12 ; Use page 12, 13 and 14 for Layer2
+
+; ---- set layer 2 clip window
+					NEXTREG REG_CLIP_WINDOW_CTRL, 1   	; reset Layer2 clip window index (to X1)
+        			NEXTREG REG_CLIP_LAYER2, 0  	    ; clip.layer2.x1 = 0
+        			NEXTREG REG_CLIP_LAYER2, $FF    	; clip.layer2.x2 = 255
+					LD 		A, (IMGNumLine)
+					DEC 	A							; If we have N lines, then last line is N-1
+        			NEXTREG REG_CLIP_LAYER2, 0	    	; clip.layer2.y1 = (IMGNumLine)
+        			NEXTREG REG_CLIP_LAYER2, A      	; clip.layer2.y2 = (IMGNumLine)
+; ---- read and set palette
+					
+					NEXTREG REG_PALETTE_CRTL, %00010000			; auto-increment ON, active palette = first layer2 pal
+					NEXTREG REG_PALETTE_INDEX,0
+					LD 		B, 0
+PaletteLoop			LD 		IX, PaletteVal
+					PUSH 	BC
+					LD 		A,(FileHandle)
+					LD      BC, 1
+					RST     $08
+					DB      F_READ          		; Read one byte
+					LD 		A, (PaletteVal)
+					NEXTREG REG_PALETTE_VALUE, A
+					POP		BC
+					DJNZ    PaletteLoop
+
+; ---- set Transparency color if needed
+					LD 		A, (TransparencyColor)
+					NEXTREG REG_TRANSPARENCY, A;      
+					OR 		A
+					CALL 	Z, FillUlaLayer			; If color is 0, we will fill the zone below the Layer2 picture with color 0 (black) so although black pixels are considered transparent, what it's seen below is also a black pixel
+					
+; ---- read pixels data
+					LD 		E,  24					; Page 12 * 2, as we access via 8K block when using MMU
+					LD 		A, (IMGNumLine)
+drawNormalLoop		OR 		A
+					JR 		Z, drawEnd
+					SUB		32
+					JR 		C, drawPartialBlock
+					PUSH 	AF
+					LD 		A, E
+					NEXTREG REG_MMU7, A				; Change MMU7 bank
+					INC 	E					    ; point to next MMU bank
+					PUSH 	DE
+					LD 		IX, $E000				; MMU 7 addres
+					LD 		BC, $2000				; 8K
+					LD 		A, (FileHandle)		
+					RST     $08
+					DB      F_READ          		; Read 8Kb
+					POP 	DE
+					POP 	AF
+					JR 		drawNormalLoop
+					
+
+					
+					LD 		IX, $E000				; MMU 7 adddress location
+					
+drawPartialBlock	ADD 	32
+					LD 		B, A
+					XOR 	A						
+					LD 		A, E
+					NEXTREG REG_MMU7, A				
+					LD 		C, 0					; BC = NumLines*256
+					LD 		IX, $E000				; MMU 7 addres
+					LD 		A, (FileHandle)		
+					RST     $08
+					DB      F_READ          		; Read 8Kb
+
+drawEnd				NEXTREG REG_MMU7, 1				; Change MMU7 bank to to original one
+
+
+; ---- Close file	
+					LD 		A,  (FileHandle)
+					RST     $08
+                    DB      F_CLOSE
+
+	
+cleanExit			EI
+					POP 	IX
+					POP 	BC
+					RET
+
+
+; Both read savegame and load savegame use the same code, that is just slightly modified before jumping in the common part at DoReadOrWrite
+
+LoadGame			LD  A, FA_READ
+					LD	(OpenMode+1),A
+					LD  A, F_READ
+					LD 	(ReadWrite),A
+					JR	DoReadOrWrite
+
+SaveGame			LD  A, FA_CREATE_AL
+					LD 	(OpenMode+1),A
+					LD  A, F_WRITE
+					LD 	(ReadWrite),A
+
+
+DoReadOrWrite		
+					EI
+ReadFilenameCall	CALL	DAAD_READ_FILENAME_ES	; Ask for file name
+					DI
+					CALL 	cleanSaveName		; Convert DAAD 10 filename into 8.3 filename
+
+; --- Set default disk  
+					CALL 	setDefaultDisk
+                    JR      C, diskFailure
+
+; --- open file
+OpenMode            LD      B, FA_READ		; May be modified by FA_CREATE_AL above
+					LD   	IX, SaveLoadFilename
+					RST     $08			
+					DB      F_OPEN      
+					JR      C, diskFailure
+					LD 	(SavegameHandle+1), A
+; --- read or write file
+
+					POP	IX			; Gets IX value back from stack, then push again
+					PUSH 	IX
+					LD 	BC, 512			; Save flags and objects
+ 					RST     $08
+ReadWrite           DB      F_READ     		; May be modified by F_WRITE ABOVE
+					JR	C, diskFailure
+                      
+
+SavegameHandle		LD 	A, $FF			; That $FF will be modifed by code above
+					RST     $08
+                    DB      F_CLOSE
+
+
+					JR 	cleanExit	
+
+diskFailure			LD 	L, 57			; E/S error
+DAADSysmesCall		CALL    DAAD_SYSMESS_ES
+					JR 	cleanExit
+			
+
+
+
+; ********************************************************************                        
+;                           AUX FUNCTIONS
+; *******************************************************************
+
+; *** Makes filename read by DAAD to be compliant with ESXDOS 8+3 filenames***
+cleanSaveName			LD 	HL, DAAD_FILENAME_ADDR_ES			; address of filename requested
+						LD 	BC, $08FF			; we will use LDI together with DJNZ, B will work for DJNZ but we set C to $FF
+						LD 	DE, SaveLoadFilename
+cleanSaveNameLoop		LD 	A, (HL)				; to avoid LDI decrement of BC to affect B
+						OR 	A
+						JR	Z, cleanSaveSetExtension
+						CP 	$20
+						JR	Z, cleanSaveSetExtension
+						LDI
+						DJNZ 	cleanSaveNameLoop
+cleanSaveSetExtension	LD 	HL, SaveLoadExtension
+						LD 	BC, 5
+						LDIR
+						RET
+
+; **** Sets the default disk active
+setDefaultDisk			XOR	A  
+                        RST     $08 
+                        DB      M_GETSETDRV
+                        RET
+			
+
+; *** Divides A by 10 and returns the remainder in A and the quotient in D^***
+DivByTen				LD 	D, A			; Does A / 10
+						LD 	E, 10			; At this point do H / 10
+						LD 	B, 8
+						XOR 	A				; A = 0, Carry Flag = 0
+DivByTenLoop			SLA	D
+						RLA			
+						CP	E		
+						JR	C, DivByTenNoSub
+						SUB	E		
+						INC	D		
+DivByTenNoSub			DJNZ DivByTenLoop
+						RET				;A= remainder, D = quotient
+
+; ** Function that patches Maluva code so it calls DAAD functions at the proper locations whe the english interpreter has been detected
+PatchForEnglish			LD HL, DAAD_READ_FILENAME_EN
+						LD (ReadFilenameCall+1), HL
+						LD HL, DAAD_SYSMESS_EN
+						LD (DAADSysmesCall+1), HL
+						LD HL, DAAD_FILENAME_ADDR_EN
+						LD(cleanSaveName+1), HL
+						LD A, $C9						; Also patch the "ask for a file name" function so it doesn't make the words be cutted between lines after calling it (patch is RET replacing a RES 6,(IX-$0a))
+						LD (DAAD_PATCH_EN), A
+						RET
+
+; ** Set attributes in ULA Layer in the zone below Layer 2 to 00, to virtually disable transparency
+FillUlaLayer		    XOR 	A
+						LD	H, A
+						LD 	A, (IMGNumLine)	; restore number of lines
+						LD 	L, A			; now HL = number of lines 
+						ADD 	HL, HL
+						ADD 	HL, HL			; Multiply by 4 (32 bytes of attributes per each 8 lines  = means 4 per line)
+						PUSH 	HL
+						POP 	BC
+						XOR 	A
+						LD (ULA_ATTR_ADDR), A	; Set first attribute to zero
+						DEC 	BC
+						LD 		HL, ULA_ATTR_ADDR
+						LD 		DE, ULA_ATTR_ADDR + 1
+						LDIR					; Copy that zero BC -1 times
+						RET
+						
+												
+
+
+Filename				DB 	"UTO.NXT",0
+TransparencyColor		DB  0
+IMGNumLine				DB 	0
+PaletteVal				DB 	0
+FileHandle				DB	0
+SaveLoadFilename		DB 	"PLACEHOLD.SAV",0
+SaveLoadExtension		DB 	".SAV", 0
+
+
