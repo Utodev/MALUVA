e88b4cf8a111311ca968dc38a4a19b8e09f5787a
Uto Dev <utodev@gmail.com>
2020-01-04 00:50:36 +0100

    Added XUNDONE condact and error reporting features

--- maluva_cp4.asm
+++ maluva_cp4.asm
@@ -18,6 +18,7 @@
 					PTR              	= $02
 					PTR2             	= $04
 					DRVNUMPLUS4      	= $AE
+					LOGICAL_FILE		= $02
 					KERNAL_SETLFS    	= $FFBA
 					KERNAL_SETNAM    	= $FFBD 
 					KERNAL_OPEN      	= $FFC0
@@ -36,6 +37,13 @@
 
 					DDB_ADDRESS 		= $3880			; DAAD DDB load address
 
+					FLAG				= $02A7 		; DAAD misc variables area
+					DONEI				= FLAG+5
+
+					USER				= DDB_ADDRESS - 514; Location of the flags
+					MALUVA_REPORT_FLAG = 20
+
+
 ; 					DAAD seems to simulate some Z80 REGS with 0-page addresses
 					BC			      	= 253
 					B       			= 254
@@ -43,6 +51,9 @@
 					HL     				= 6 ; it was 251
 					H      				= 7 ; it was 252
 					L      				= 6 ; it was 251
+					DE      			= 139
+					E      				= 139
+					D      				= 140
 
 ; 					Other Page0 addresses usable for a while
 
@@ -59,18 +70,24 @@
 Start				PHP							; Save status register
 					SEI 						; Disable interrupt
 					STA     Registro1
+
+					LDA 	USER+MALUVA_REPORT_FLAG		; Clear Maluva Report flag bit 7
+					AND     #$7F
+					STA 	USER+MALUVA_REPORT_FLAG
+
 					TYA
 					LDX     Registro1            ; Now X has first parameter, and A has second (function number)
 					CMP 	#0
 					BEQ		LoadImg
+					CMP 	#3
+					BEQ 	Xmessage
 					CMP 	#4
 					BEQ     XPart
+					CMP 	#7
+					BEQ     XUndone
 					CMP		#255
 					BEQ 	restoreXmessage
-					CMP 	#3
-					BEQ 	+
-					JMP		CleanExit
-+					JMP 	Xmessage				
+					JMP		ExitWithError2
 					
 
 ; ---- Set the filename
@@ -98,10 +115,10 @@ LoadImg				TXA							; Move first parameter (image number) to A
 					LDA     #5
 					JSR 	OpenFile
 
-					JSR KERNAL_CHRIN		; Read number of attribute lines
-					STA Registro1			; Save number of attr lines
-					JSR KERNAL_READST		; Read file status
-        			BNE Eof      			; either EOF or read error, leave
+ReadFile			JSR KERNAL_CHRIN				; Read number of attribute lines
+					STA Registro1					; Save number of attr lines
+					JSR KERNAL_READST				; Read file status
+        			BNE ExitWithError				; either EOF or read error, leave and don't close cause it was likely not opened
 					LDA Registro1
 					PHA
 
@@ -153,21 +170,96 @@ LoadAttrs			LDA #<PLUS4_ATTRS_RAM
 					JSR ReadCompressedBlock
 
 					LDA #$20
-					STA PatchJSR1           ; Attributes had been loaded once at least, so we can call "ConvertColors", change BIT to JST below
+					STA PatchJSR1           ; Attributes had been loaded once at least, so we can call "ConvertColors", change BIT to JSR below
 
-Eof        			LDA #$02
-        			JSR KERNAL_CLOSE  		; close file
-        			JSR KERNAL_CLRCHN 		; restore input to default channel (keyboard)
+;--- And close file					
+					JSR CloseFile
 
-
-CleanExit			STA PLUS4_PAGE_RAM	
+CleanExit  			STA PLUS4_PAGE_RAM	
 PatchJSR1			BIT ConvertColors
+					LDA #$2c
+					STA PatchJSR1           ; Once ConvertColors has been called, when change again JSR to BIT, so it's not called for other functions
+					
 					LDA #$3b
         			STA PLUS4_SCREEN_CTR
                     LDA #$2c
-					STA PatchJSR1           ; Once ConvertColors has been called, when change again JST to BIT, so it's not called for other functions
 					PLP						; Restore status register (and previous interrupt status as interrupt status is a flag just like Z or C)
 					RTS
+
+CloseFile			LDA #LOGICAL_FILE
+        			JSR KERNAL_CLOSE  		; close file
+					JSR KERNAL_CLRCHN 		; restore input to default channel (keyboard)
+        			RTS					
+
+
+
+cleanExitNotdone	STA PLUS4_PAGE_RAM	
+					LDA #$3b
+        			STA PLUS4_SCREEN_CTR
+					PLP						
+
+					PLA 				; Get LSB of return address
+					STA E
+					PLA 
+					STA D
+					JSR IncDE			; First we increase return address by one cause JSR does not stack the return address, but return address-1 (RTS is the one that increases the value)
+					JSR IncDE			; The we increment once more to step over the JMP opcode and point to where NXTOP is actually stored in RAM
+					LDY #0
+					LDA (DE), Y
+					STA L
+					JSR IncDE
+					LDA (DE), Y
+					STA H
+					LDY #14
+-					JSR IncHL			; IncHL 14 times
+					DEY
+					BNE -
+					LDA L
+					STA PatchNXTOPJMP+1
+					LDA H
+					STA PatchNXTOPJMP+2
+
+FakeNXTOP			JSR IncBC
+					PLA 
+					STA L
+					PLA
+					STA H
+					CLI					; EI is not in NXTOP but it was in Cleanexit
+PatchNXTOPJMP		JMP		0			; This will be patched above
+
+
+
+; Useful functions
+IncHL   			INC     L
+        			BNE     +
+        			INC     H
++			        RTS
+
+IncBC   			INC     C
+        			BNE     +
+        			INC     B
++			        RTS
+
+
+IncDE   			INC     E
+        			BNE     +
+        			INC     D
++			        RTS
+
+ExitWithError  			JSR 	CloseFile
+ExitWithError2			LDA 	USER+MALUVA_REPORT_FLAG			; Sets bit 7 of Maluva report flag
+						ORA     #$80
+						STA 	USER+MALUVA_REPORT_FLAG
+						AND 	#1								; checks if Maluva report flag first bit is 1
+						BNE 	cleanExitNotdone 				; if so, exits without setting done status
+						JMP		cleanExit
+
+
+
+XUndone				LDA #0
+					STA DONEI
+					JMP cleanExitNotdone
+
 
 
 ; ----------------------------- Xpart --- Handles wich part (part 1 or part 2 of the game is being played)
@@ -238,34 +330,32 @@ FakeCondacts		.byte 	$36, 0,	$3D, 0, $FF ; SYSMESS 0 EXTERN 0 255
 OpenFile			STY RegistroY
 					STX RegistroX
 					PHA
-
         			LDA #$0b
         			STA PLUS4_SCREEN_CTR	 
         			STA PLUS4_PAGE_ROM       
-
 					LDA #0
 					STA SecondaryAddress+1  ; SETLFS for open
 					PLA
 					LDX RegistroX
 					LDY RegistroY
 		 			JSR KERNAL_SETNAM
-        			LDA #$02				; Logical number
-        			LDX DRVNUMPLUS4       		; last used device number
+        			LDA #LOGICAL_FILE		; Logical number
+        			LDX DRVNUMPLUS4       	; last used device number
         			BNE SecondaryAddress
         			LDX #$08      			; default to device 8
-SecondaryAddress	LDY #$00      			; not $01 means: load to address stored in file
+SecondaryAddress	LDY #0					; not $01 means: load to address stored in file
         			JSR KERNAL_SETLFS
 					JSR KERNAL_OPEN 		; open file
-        			BCC +
-					PLA						; Just to clear stack return value
-					JMP CleanExit
-+					LDX #$02				; Use file #2 for input/output
+					BCS OpenError
+					LDX #LOGICAL_FILE		; Use file for input/output
         			JSR KERNAL_CHKIN		; Set input to file
 					JSR KERNAL_READST
-        			BNE OpenFileError		; either EOF or read error
+					BNE OpenError
 					RTS
-OpenFileError		PLA						; Just to clear stack return value
-					JMP Eof					
+
+OpenError			PLA 
+					PLA					
+					JMP ExitWithError2
 
 ; Clears Mem at ($AE), as many bytes as the value received in X multuplied by 40, and filling with the value at Y
 ClearMem			STY ClearValue+1
@@ -288,30 +378,29 @@ ClrDoNotIncMSBAttr  DEC Registro1
 
 ; Reads RLE compressed data from disk and stores it at ($AE) until a given value with zero repeats appears
 ReadCompressedBlock JSR KERNAL_READST
-        			BNE Eof      			; either EOF or read error
-					JSR KERNAL_CHRIN		; Read repeat value
-					STA CompressedCMP+1     ; Save repeat Value self-modifying the code
+        			BNE ExitWithError  			; either EOF or read error
+					JSR KERNAL_CHRIN			; Read repeat value
+					STA CompressedCMP+1     	; Save repeat Value self-modifying the code
 
 
-CompressedLoop		JSR KERNAL_READST		; Read file status
-        			BNE Eof      			; either EOF or read error, leave
-					JSR KERNAL_CHRIN    	; Read value
-CompressedCMP		CMP #0					; This #0 value is replaced above with the "repeat follows" byte. If found two bytes follow (repeats, value)
+CompressedLoop		JSR KERNAL_READST			; Read file status
+        			BNE ExitWithError			; either EOF or read error, leave
+					JSR KERNAL_CHRIN    		; Read value
+CompressedCMP		CMP #0						; This #0 value is replaced above with the "repeat follows" byte. If found two bytes follow (repeats, value)
 					BNE CompressNoRep
-					JSR KERNAL_READST		; Read file status
-        			BNE Eof      			; either EOF or read error, leave
-					JSR KERNAL_CHRIN    	; Read number of repeats
-					CMP #0        			; if zero repeats, it's end of block
+					JSR KERNAL_READST			; Read file status
+        			BNE ExitWithError			; either EOF or read error, leave
+					JSR KERNAL_CHRIN    		; Read number of repeats
+					CMP #0        				; if zero repeats, it's end of block
 					BNE CompresRep
-					RTS						; End of block found
+					RTS							; End of block found
 
-CompresRep          STA Registro1           ; Preserve repeats
-					JSR KERNAL_READST		; Read file status
-        			BNE Eof      			; either EOF or read error, leave
-					JSR KERNAL_CHRIN    	; Read value to repeat
-					LDX Registro1           ; Restore number of repeats
+CompresRep          STA Registro1           	; Preserve repeats
+					JSR KERNAL_READST			; Read file status
+        			BNE ExitWithError			; either EOF or read error, leave
+					JSR KERNAL_CHRIN    		; Read value to repeat
+					LDX Registro1           	; Restore number of repeats
 					JMP RepeatLoop
-
 
 CompressNoRep		LDX #1					; If not repeated, value is repeated just once
 
@@ -435,7 +524,9 @@ XMessage			STX L
 
 					LDA H          			; HL now has the real offset within the file, but with MSB (H) increased by 1, which will make the DEC H down here end loop on first decrement
 					INC H					; otherwise, first decrement would turn H int $FF, not zero 
--					JSR KERNAL_CHRIN		
+-					JSR KERNAL_CHRIN	
+					JSR KERNAL_READST
+					BNE ExitWithError
 					DEC L
 					BNE -
 					DEC H
@@ -450,6 +541,10 @@ ReadMsg				LDA <#XmessageBuffer   ; LSB of XMessageBuffer
 
 ReadMsgLoop			JSR KERNAL_CHRIN		; Read number of attribute lines
 					TAX
+					JSR KERNAL_READST
+					AND #$BF					; Discard EOF error
+					BNE ExitWithError
+					TXA
 					EOR #$FF
 					CMP #10					; End of message mark
 					BEQ TextLoaded
@@ -473,7 +568,8 @@ XmessPrintMsg		JSR 	preserveBC
 					STA 	C
 					LDA	 	>#FakeCondacts-1
 					STA 	B
-					JMP Eof
+					JSR 	CloseFile
+					JMP 	cleanExit
 
 ; ------------------------------- Variables and tables  -----------------
 
